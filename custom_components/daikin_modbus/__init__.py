5d729dc4e7c3c5f5f0cf03119a342cfca7e2301d
jayg37 <jayg37@hotmail.com>
2026-09-25 07:54:46 -0400

    Use HA Modbus shared unit API

--- custom_components/daikin_modbus/__init__.py
+++ custom_components/daikin_modbus/__init__.py
@@ -2,12 +2,13 @@
 
 from .vendor.daikin_modbus import DaikinAidoo
 
-from homeassistant.components.modbus_connection import async_get_unit
+from homeassistant.components.modbus import async_get_unit
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from modbus_connection import ModbusTcpParams
 
-from .const import CONF_CONNECTION, CONF_UNIT_ID
+from .const import CONF_HOST, CONF_PORT, CONF_UNIT_ID
 from .coordinator import DaikinCoordinator
 
 PLATFORMS = [Platform.CLIMATE, Platform.SENSOR]
@@ -16,16 +17,19 @@ type DaikinConfigEntry = ConfigEntry[DaikinCoordinator]
 
 async def async_setup_entry(hass: HomeAssistant, entry: DaikinConfigEntry) -> bool:
     """Set up the Daikin Modbus integration."""
-    unit = async_get_unit(hass, entry.data[CONF_CONNECTION], int(entry.data[CONF_UNIT_ID]))
+    unit = async_get_unit(
+        hass,
+        entry,
+        ModbusTcpParams(
+            host=entry.data[CONF_HOST],
+            port=int(entry.data[CONF_PORT]),
+        ),
+        int(entry.data[CONF_UNIT_ID]),
+    )
     device = DaikinAidoo(unit)
     coordinator = DaikinCoordinator(hass, entry, device)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
-    entry.async_on_unload(
-        unit.on_connection_lost(
-            lambda: hass.config_entries.async_schedule_reload(entry.entry_id)
-        )
-    )
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
