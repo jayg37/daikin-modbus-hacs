3df154a671411fb506835dfaa2588b8bf55ae443
jayg37 <jayg37@hotmail.com>
2026-09-25 07:54:44 -0400

    Update Daikin Modbus connection constants

--- custom_components/daikin_modbus/const.py
+++ custom_components/daikin_modbus/const.py
@@ -3,7 +3,10 @@
 from datetime import timedelta
 
 DOMAIN = "daikin_modbus"
-CONF_CONNECTION = "connection"
+CONF_HOST = "host"
+CONF_PORT = "port"
 CONF_UNIT_ID = "unit_id"
+DEFAULT_HOST = "192.168.1.29"
+DEFAULT_PORT = 502
 DEFAULT_UNIT_ID = 1
 SCAN_INTERVAL = timedelta(seconds=30)
